fa85018294b07e6c359a1ec0a88907bc7246212a
Sam Corcos <sam@corcos.io>
2015-09-03 00:28:24 +0200

    boxes in component

--- client/components/Modal.jsx
+++ client/components/Modal.jsx
@@ -3,7 +3,30 @@ Modal = React.createClass({
     return (
       <div className="modal-active-darken">
         <div className="modal-container">
-          
+          <div id="modal-content"></div>
+          <AddVolunteerModalContent />
+        </div>
+      </div>
+    )
+  }
+})
+
+AddVolunteerModalContent = React.createClass({
+  render() {
+    return (
+      <div>
+        <div className="volunteer-form-row">
+          <div className="volunteer-form-column-60">
+
+          </div>
+          <div className="volunteer-form-column-40">
+
+          </div>
+        </div>
+        <div className="volunteer-form-row">
+          <div className="volunteer-form-column-100">
+
+          </div>
         </div>
       </div>
     )
