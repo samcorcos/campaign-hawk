e2632aaa39135f66f890c328939a7c25598fe4a8
Sam Corcos <sam@corcos.io>
2015-09-02 23:30:32 +0200

    styling modal

--- client/components/Modal.jsx
+++ client/components/Modal.jsx
@@ -2,7 +2,9 @@ Modal = React.createClass({
   render() {
     return (
       <div className="modal-active-darken">
-
+        <div className="modal-container">
+          
+        </div>
       </div>
     )
   }
