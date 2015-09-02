6ed48b5763f407dd53854567e6eda7fc92743245
Sam Corcos <sam@corcos.io>
2015-09-02 16:37:37 +0200

    adding fontawesome, sidenav styling

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -12,9 +12,30 @@ Map = React.createClass({
 
 Sidenav = React.createClass({
   render() {
+    console.log("rendering list of icons");
+    let iconList = [
+      "fa fa-database",
+      "fa fa-user-plus",
+      "fa fa-users",
+      "fa fa-bicycle",
+      "fa fa-list-ul",
+      "fa fa-lightbulb-o",
+      "fa fa-list-ol",
+      "fa fa-line-chart",
+      "fa fa-cog"
+    ]
+    let list = iconList.map((item) => {
+      return (
+        <li key={item} className="sidenav-list-item">
+          <i className={item}></i>
+        </li>
+      )
+    })
     return (
       <nav className="sidenav">
-        icons
+        <ul className="sidenav-list">
+          {list}
+        </ul>
       </nav>
     )
   }
